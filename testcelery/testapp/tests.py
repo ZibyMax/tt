51903198dcc560bf88b159e735432b33aa011ba8
ZibyMax <max@rekstar-nn.ru>
2020-01-20 19:06:23 +0300

    тесты заработали

--- testcelery/testapp/tests.py
+++ testcelery/testapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestClass(TestCase):
+
+    def test1(self):
+        self.assertTrue(True)
+
+    def test2(self):
+        self.assertEqual(1, 2)
+
--- testcelery/testapp/views.py
+++ testcelery/testapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def test(request):
+    return Response({'message': 'test passed'})
+
